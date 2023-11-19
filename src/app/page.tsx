bfcf8dfa56f0387dd6854367dab4c400e4c96823
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2023-11-19 18:27:21 +0530

    feat: Add Welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import Welcome from "./Welcome";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>Hello! Programmers</h1>
-    </main>
-  );
+  let isLogin = false;
+  let app: JSX.Element;
+
+  if (isLogin) {
+    app = (
+      <main>
+        <h1>Home Page</h1>
+      </main>
+    );
+  } else {
+    app = <Welcome />;
+  }
+
+  return app;
 }
