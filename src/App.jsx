90375f5bfd63667a6d6120f1988be1e650830c01
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-05-25 12:38:16 +0530

    fix(theme): theme applied to the whole app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import {Routes, Route} from 'react-router-dom';
-
-import {createTheme, ThemeProvider} from '@mui/material';
-import CssBaseline from '@mui/material/CssBaseline'; // Import CssBaseline for consistent styling
-import {purple} from '@mui/material/colors';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Otp from './pages/Otp';
@@ -14,90 +10,6 @@ import useFetchUserData from './hooks/useFetchUserData';
 import Home from './components/Home';
 import Loader from './components/Loader';
 
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-    primary: purple,
-    secondary: {
-      main: '#757575',
-      contrastText: '#ffffff',
-    },
-    text: {
-      primary: '#212121',
-      secondary: '#b0bec5',
-      disabled: '#757575',
-    },
-    error: {
-      main: '#f44336',
-    },
-    warning: {
-      main: '#ffa726',
-    },
-    info: {
-      main: '#29b6f6',
-    },
-    success: {
-      main: '#66bb6a',
-    },
-    divider: '#b0bec5',
-  },
-  typography: {
-    fontFamily: 'Quicksand',
-    fontWeightLight: 400,
-    fontWeightRegular: 500,
-    fontWeightMedium: 600,
-    fontWeightBold: 700,
-    h1: {
-      color: '#212121',
-    },
-    h2: {
-      color: '#212121',
-    },
-  },
-});
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: purple,
-    secondary: {
-      main: '#8687E780',
-      contrastText: '#ffffff',
-    },
-    text: {
-      primary: '#ffffff',
-      secondary: '#b0bec5',
-      disabled: '#757575',
-    },
-    error: {
-      main: '#f44336',
-    },
-    warning: {
-      main: '#ffa726',
-    },
-    info: {
-      main: '#29b6f6',
-    },
-    success: {
-      main: '#66bb6a',
-    },
-    divider: '#b0bec5',
-  },
-  typography: {
-    fontFamily: 'Quicksand',
-    fontWeightLight: 400,
-    fontWeightRegular: 500,
-    fontWeightMedium: 600,
-    fontWeightBold: 700,
-    h1: {
-      color: '#ffffff',
-    },
-    h2: {
-      color: '#ffffff',
-    },
-  },
-});
-
 function App() {
   const loading = useFetchUserData();
 
@@ -106,32 +18,23 @@ function App() {
   }
 
   return (
-    <ThemeProvider
-      theme={
-        window.matchMedia('(prefers-color-scheme: dark)').matches
-          ? darkTheme
-          : lightTheme
-      }>
-      <CssBaseline />
-
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          {/* public routes */}
-          <Route element={<NoAuth />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="otp" element={<Otp />} />
-          </Route>
-
-          {/* protected routes */}
-          <Route element={<RequireAuth />}>
-            <Route path="/" element={<Home />} />
-          </Route>
-
-          <Route path="*" element={<NotFound />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route element={<NoAuth />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="otp" element={<Otp />} />
         </Route>
-      </Routes>
-    </ThemeProvider>
+
+        {/* protected routes */}
+        <Route element={<RequireAuth />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
