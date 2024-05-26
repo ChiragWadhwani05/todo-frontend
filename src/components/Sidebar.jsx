a5ce3e12f940b53fabe09981866f9feb253eaed0
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-05-26 21:42:36 +0530

    feat(todo): Implement todo api in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,9 +16,14 @@ import ContrastOutlinedIcon from '@mui/icons-material/ContrastOutlined';
 import ListItemButton from '@mui/material/ListItemButton';
 import Checkbox from '@mui/material/Checkbox';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { useDispatch, useSelector } from 'react-redux';
-import { selectCurrentUser } from '../features/auth/authSlice';
-import { toggleProfileDialog } from '../features/misc/dialogs';
+import {useDispatch, useSelector} from 'react-redux';
+import {selectCurrentUser} from '../features/auth/authSlice';
+import {toggleProfileDialog} from '../features/misc/dialogs';
+import {selectAllTodo} from '../features/todos/todosSlice';
+import {
+  useCreateTodoMutation,
+  useDeleteTodoMutation,
+} from '../api/auth/todoApiSlice';
 
 const drawerWidth = 300;
 
@@ -52,77 +57,12 @@ function stringAvatar(name) {
 const Sidebar = () => {
   const user = useSelector(selectCurrentUser);
   const [checked, setChecked] = React.useState([0]);
-  const dummyTodos = [
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=1',
-      title: 'Finish React Project',
-      description:
-        'Complete the frontend for the new React project by the end of the week.',
-      isComplete: false,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=2',
-      title: 'Grocery Shopping',
-      description:
-        'Buy groceries for the week including fruits, vegetables, and dairy products.',
-      isComplete: true,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=3',
-      title: 'Workout Session',
-      description:
-        'Attend the gym for a workout session focusing on strength training and cardio.',
-      isComplete: false,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=4',
-      title: 'Read a Book',
-      description: 'Read at least 50 pages of the current book I am reading.',
-      isComplete: true,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=5',
-      title: 'Team Meeting',
-      description:
-        'Participate in the weekly team meeting to discuss project updates and next steps.',
-      isComplete: false,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=6',
-      title: 'Call with Client',
-      description:
-        'Have a conference call with the client to discuss their requirements and feedback.',
-      isComplete: true,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=7',
-      title: 'Plan Weekend Trip',
-      description:
-        'Plan the itinerary and book accommodations for the weekend trip.',
-      isComplete: false,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=8',
-      title: 'Complete Online Course',
-      description:
-        'Finish the modules and assignments for the online course on Data Science.',
-      isComplete: true,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=9',
-      title: 'Cook Dinner',
-      description: 'Prepare a healthy dinner for the family with a new recipe.',
-      isComplete: false,
-    },
-    {
-      coverImage: 'https://source.unsplash.com/random/400x200?sig=10',
-      title: 'Update Resume',
-      description:
-        'Revise and update my resume with the latest work experience and skills.',
-      isComplete: true,
-    },
-  ];
-  const handleToggle = (value) => () => {
+  const todos = useSelector(selectAllTodo);
+  const [createTodo, {isLoading}] = useCreateTodoMutation();
+  const [deleteTodo] = useDeleteTodoMutation();
+  const [todosToDelete, setTodosToDelete] = React.useState(new Set());
+
+  const handleToggle = value => () => {
     const currentIndex = checked.indexOf(value);
     const newChecked = [...checked];
 
@@ -150,9 +90,8 @@ const Sidebar = () => {
         },
       }}
       variant="permanent"
-      anchor="left"
-    >
-      <Box sx={{ p: 2, display: 'flex', alignItems: 'center' }}>
+      anchor="left">
+      <Box sx={{p: 2, display: 'flex', alignItems: 'center'}}>
         <Button
           sx={{
             display: 'flex',
@@ -160,32 +99,34 @@ const Sidebar = () => {
             cursor: 'pointer',
             textTransform: 'none',
           }}
-          onClick={handleProfileClick}
-        >
+          onClick={handleProfileClick}>
           {user.avatar ? (
-            <Avatar src={user.avatar} sx={{ width: 30, height: 30, mr: 1 }} />
+            <Avatar src={user.avatar} sx={{width: 30, height: 30, mr: 1}} />
           ) : (
             <Avatar
               {...stringAvatar(
-                `${user.givenName} ${user.familyName ? user.familyName : ''}`
+                `${user.givenName} ${user.familyName ? user.familyName : ''}`,
               )}
-              sx={{ width: 30, height: 30, mr: 1 }}
+              sx={{width: 30, height: 30, mr: 1}}
             />
           )}
-          <Typography variant="h6" component="div" sx={{ color: '#FFFFFF' }}>
+          <Typography variant="h6" component="div" sx={{color: '#FFFFFF'}}>
             {user.givenName} {user.familyName}
           </Typography>
         </Button>
-        <IconButton sx={{ marginLeft: 'auto' }}>
+        <IconButton sx={{marginLeft: 'auto'}}>
           <ContrastOutlinedIcon />
         </IconButton>
       </Box>
 
-      <Box sx={{ p: 2 }}>
+      <Box sx={{p: 2}}>
         <Button
           startIcon={<AddCircleIcon />}
-          sx={{ width: '100%', textTransform: 'none' }}
-        >
+          sx={{width: '100%', textTransform: 'none'}}
+          onClick={async () =>
+            await createTodo({title: 'New Task', description: 'desc'})
+          }
+          disabled={isLoading}>
           <Typography variant="body1" fontWeight={700}>
             Add Task
           </Typography>
@@ -193,34 +134,47 @@ const Sidebar = () => {
       </Box>
 
       <Box>
-        <List
-          sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}
-        >
-          {dummyTodos.map((value) => {
-            const labelId = `checkbox-list-label-${value}`;
+        <List sx={{width: '100%', maxWidth: 360, bgcolor: 'background.paper'}}>
+          {Object.values(todos).map(todo => {
+            const labelId = `checkbox-list-label-${todo._id}`;
 
             return (
               <ListItem
-                key={value.title}
+                key={todo._id}
                 secondaryAction={
-                  <IconButton edge="end" aria-label="comments">
+                  <IconButton
+                    edge="end"
+                    aria-label="comments"
+                    onClick={async () => {
+                      setTodosToDelete(curTodosToDelete => {
+                        const newTodosToDelete = new Set(curTodosToDelete);
+                        newTodosToDelete.add(todo._id);
+                        return newTodosToDelete;
+                      });
+                      await deleteTodo({_id: todo._id});
+                      setTodosToDelete(curTodosToDelete => {
+                        const newTodosToDelete = new Set(curTodosToDelete);
+                        newTodosToDelete.delete(todo._id);
+                        return newTodosToDelete;
+                      });
+                    }}
+                    disabled={todosToDelete.has(todo._id)}>
                     <DeleteIcon />
                   </IconButton>
                 }
-                disablePadding
-              >
+                disablePadding>
                 <ListItemButton role={undefined} dense>
                   <ListItemIcon>
                     <Checkbox
                       edge="start"
-                      checked={value.isComplete}
+                      checked={todo.isComplete}
                       tabIndex={-1}
                       disableRipple
-                      inputProps={{ 'aria-labelledby': labelId }}
-                      onClick={handleToggle(value)}
+                      inputProps={{'aria-labelledby': labelId}}
+                      onClick={handleToggle(todo)}
                     />
                   </ListItemIcon>
-                  <ListItemText id={labelId} primary={value.title} />
+                  <ListItemText id={labelId} primary={todo.title} />
                 </ListItemButton>
               </ListItem>
             );
